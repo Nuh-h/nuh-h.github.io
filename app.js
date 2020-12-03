adbcd6789c4aa084b99da5317f4b45a46cfd8b64
Nuh <69435206+Nuh-h@users.noreply.github.com>
2020-12-03 11:42:50 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-//const blankList = document.querySelector('main');
-//console.log(blankList)
+// var mayn = document.body.firstElementChild;
+// var elemDiv = document.createElement('div');
+// elemDiv.style.cssText = 'position:absolute;width:88%;height:60%;opacity:0.3;background:#000; color:white; font-size:25px;';
+// mayn.appendChild(elemDiv);
 
-var mayn = document.body.firstElementChild;
-var elemDiv = document.createElement('div');
-elemDiv.style.cssText = 'position:absolute;width:88%;height:60%;opacity:0.3;background:#000; color:white; font-size:25px;';
-mayn.appendChild(elemDiv);
-
-var listWrap = mayn.lastElementChild;
-listWrap.innerHTML += "<p>Hello jcoders!</p>";
-listWrap.innerHTML+='<button onclick="myFunction()">Press!</button>';
-function myFunction(){
-	alert("HELLO, you got here nicely!");
-}
+// var listWrap = mayn.lastElementChild;
+// listWrap.innerHTML += "<p>Hello jcoders!</p>";
+// listWrap.innerHTML+='<button onclick="myFunction()">Press!</button>';
+// function myFunction(){
+// 	alert("HELLO, you got here nicely!");
+// }
 function dropdown(value){
 	if(value==1){ var first = document.querySelector('.first');}
 	else if(value==2){ var first = document.querySelector('.second'); }
 	else{ var first = document.querySelector('.third');}
 	first.classList.toggle('disappear');
 }
-
-//	mobileMenu.addEventListener("click", () => {
-//	menuItems.classList.toggle("open");
-//	menuItem.forEach(link => {link.classList.toggle("fade");});
-//	});
-
-// 	console.log("mobileMenu is empty");
-// 	console.log(menuItems.classList);
-
-//const gitlink = document.querySelector('a.git-link');
-//gitlink.addEventListener("click", ()=> {alert("Link broken, will be fixed soon");});
