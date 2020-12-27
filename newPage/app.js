c0a831e2299610a513c45b96d1a8d38c6e8b2618
Nuh <69435206+Nuh-h@users.noreply.github.com>
2020-12-27 20:29:26 +0000

    Update app.js

--- newPage/app.js
+++ newPage/app.js
@@ -1,11 +1,10 @@
-function myMove() {
-  var elem = document.getElementsByTag("main")[0].firstElementChild;   
-  const sentence = "Hi\nMy name is Nuh\nI like to code\nto help make this world a better, fairer place\nI have personal experience\nthat drives my professional passion\nI hope to become an excellent engineer\nthat can lead teams\nto build innovative solutions\nthat make a difference.\nThank you for your patience,\nnice to meet you.";
-  const sentenceArray = sentence.split('\n');
-  //console.log(sentenceArray);
-  const delay = ms => new Promise(res => setTimeout(res, ms));
-  const yourFunction = async () => {
-    for(var word of sentenceArray){
+var elem = document.getElementsByTag("main")[0].firstElementChild;   
+const sentence = "Hi\nMy name is Nuh\nI like to code\nto help make this world a better, fairer place\nI have personal experience\nthat drives my professional passion\nI hope to become an excellent engineer\nthat can lead teams\nto build innovative solutions\nthat make a difference.\nThank you for your patience,\nnice to meet you.";
+const sentenceArray = sentence.split('\n');
+//console.log(sentenceArray);
+const delay = ms => new Promise(res => setTimeout(res, ms));
+const playIntro = async () => {
+  for(var word of sentenceArray){
       elem.innerHTML="";
       await delay(500);
       console.log(word);
@@ -13,8 +12,6 @@ function myMove() {
         elem.innerHTML+=letter;
         await delay(200);
       }
-    }
-  };
-  yourFunction();
-}
-setInterval(myMove, 60000);
+  }
+};
+setInterval(playIntro(), 60000);
