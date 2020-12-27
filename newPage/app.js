d22e4399108380797629001cb7509b91cde9e0f6
Nuh <69435206+Nuh-h@users.noreply.github.com>
2020-12-27 20:31:24 +0000

    Update app.js

--- newPage/app.js
+++ newPage/app.js
@@ -1,4 +1,4 @@
-var elem = document.getElementsByTag("main")[0].firstElementChild;   
+var elem = document.getElementsByTagName("main")[0].firstElementChild;   
 const sentence = "Hi\nMy name is Nuh\nI like to code\nto help make this world a better, fairer place\nI have personal experience\nthat drives my professional passion\nI hope to become an excellent engineer\nthat can lead teams\nto build innovative solutions\nthat make a difference.\nThank you for your patience,\nnice to meet you.";
 const sentenceArray = sentence.split('\n');
 //console.log(sentenceArray);
