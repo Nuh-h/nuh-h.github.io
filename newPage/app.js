7e60af5b15699f9e7ae261e542c6ee987b46047b
Nuh <69435206+Nuh-h@users.noreply.github.com>
2020-12-27 23:01:37 +0000

    Update app.js

--- newPage/app.js
+++ newPage/app.js
@@ -1,17 +1,20 @@
-var elem = document.getElementsByTagName("main")[0].firstElementChild;   
-const sentence = "Hi\nMy name is Nuh\nI like to code\nto help make this world a better, fairer place\nI have personal experience\nthat drives my professional passion\nI hope to become an excellent engineer\nthat can lead teams\nto build innovative solutions\nthat make a difference.\nThank you for your patience,\nnice to meet you.";
-const sentenceArray = sentence.split('\n');
-//console.log(sentenceArray);
-const delay = ms => new Promise(res => setTimeout(res, ms));
-const playIntro = async () => {
-	for(var word of sentenceArray){
-		elem.innerHTML="";
-		await delay(500);
-		console.log(word);
-		for(var letter of word){
-			elem.innerHTML+=letter;
-			await delay(200);
-		};
-	};
+var play = ()=> {
+    var elem = document.getElementsByTagName("main")[0].firstElementChild;   
+    var sentence = "Hi\nMy name is Nuh\nI like to code\nto help make this world a better, fairer place\nI have personal experience\nthat drives my professional passion\nI hope to become an excellent engineer\nthat can lead teams\nto build innovative solutions\nthat make a difference.\nThank you for your patience,\nnice to meet you.";
+    var sentenceArray = sentence.split('\n');
+    //console.log(sentenceArray);
+    var delay = ms => new Promise(res => setTimeout(res, ms));
+    var playIntro = async () => {
+        for(var word of sentenceArray){
+            elem.innerHTML="";
+            await delay(500);
+            console.log(word);
+            for(var letter of word){
+                elem.innerHTML+=letter;
+                await delay(200);
+            };
+        };
+    };
+    playIntro();
 };
-setInterval(playIntro(), 30000);
+setInterval(play(),70000)//every 1 minute and 10 seconds
