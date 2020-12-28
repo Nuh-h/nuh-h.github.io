5e7b155e7c4de5fe713cb88ca813c41696a94672
Nuh <69435206+Nuh-h@users.noreply.github.com>
2020-12-28 01:27:18 +0000

    Update app.js

--- newPage/app.js
+++ newPage/app.js
@@ -1,6 +1,9 @@
-    var play = ()=> {
-        var elem = document.getElementsByTagName("p")[0];   
+var replayBtn = document.createElement('span');
+replayBtn.addEventListener('click',()=>{play()});    
+var play = ()=> {
+        var elem = document.getElementsByTagName("p")[0];         var main = document.getElementsByTagName('main')[0];
         elem.innerHTML="";
+        if(main.contains(replayBtn)){ main.removeChild(replayBtn); }
         var sentence = "Hi....\nMy name is Nuh\nI like to code\nto help make this world a better, fairer place\nI have personal experience\nthat drives my professional passion\nI hope to become an excellent engineer\nthat can lead teams\nto build innovative solutions\nthat make a difference.\nThank you for your patience,\nnice to meet you.";
         var sentenceArray = sentence.split('\n');
         //console.log(sentenceArray);
@@ -15,11 +18,8 @@
                 elem.innerHTML+="<br>";
             };
             await delay(100);
-            var rep = document.createElement('span');
-            //rep.classList.add('replay');
-            rep.addEventListener('click',()=>{play()});
-            elem.prepend(rep);
-            //elem.innerHTML = rep.innerHTML+elem.innerHTML;
+            main.prepend(replayBtn);
+
         };
         playIntro();
     };
