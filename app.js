999217262954c50648c55076cda57a3f6cb6c4c8
Nuh <69435206+Nuh-h@users.noreply.github.com>
2021-01-09 00:24:17 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ var play = ()=> {
 		var intro = document.querySelector('.intro');
         elem.innerHTML="";
         if(intro.contains(replayBtn)){ intro.removeChild(replayBtn); }
-        //var sentence = "Hi,\ \nMy name is Nuh.\nI like to code and to learn,\nto help others and make a difference.\nI am aiming to become an excellent engineer \nthat can also lead teams to build innovative \nand impactful solutions.\n This is my newly built website,\n you are welcome to have an early browse.\n Thank you for your patience,\n and nice to meet you.";
         var sentence = "Hi \n My name is Nuh\n Welcome to my newly built website\n I love challenges \n from hiking and horse riding \n to coding for the world\n I also love learning about cultures \n I look for similarities\n to appreciate the differences.\n Do you?"
 		var sentenceArray = sentence.split('\n');
-        //console.log(sentenceArray);
+
         var delay = ms => new Promise(res => setTimeout(res, ms));
         var playIntro = async () => {
 			var i=0;
@@ -18,8 +17,6 @@ var play = ()=> {
             for(var word of sentenceArray){
                 console.log(word);
 				i++;
-				//var line = document.createElement('p');
-				//intro.appendChild(line);
                 for(var letter of word){
                     elem.innerHTML+=letter;
                     await delay(80);
@@ -30,9 +27,7 @@ var play = ()=> {
 				for(var letter of word){
                     elem.innerHTML=elem.innerHTML.substring(0,elem.innerHTML.length-1);
                     await delay(50);
-                };
-                //elem.innerHTML+="<br>";
-				
+                };				
             };
             await delay(10000);
             //intro.prepend(replayBtn);
@@ -40,8 +35,52 @@ var play = ()=> {
         };
         playIntro();
 };
+var blogResponse;
 if(document.title==="Home" && document.readyState){play();}
+if(document.title==="Blog" && document.readyState){	postBlogs();}
+function postBlogs(){
+	var blogPosts = document.querySelector('.blog-posts');
+	blogPosts.innerHTML="";
 
+	fetch('https://nuh-h.github.io/blogposts.json').then(response => response.json()).then(jsonResponse => {
+		jsonResponse.posts.forEach(item=>{
+			var blogPosts = document.querySelector('.blog-posts');
+			var nPost = document.createElement('div');
+			nPost.classList.add('post');
+				
+			var nDate = document.createElement('div');
+			nDate.classList.add('post-date');
+			nDate.innerHTML=item.date;
+				
+			var nPostContainer = document.createElement('div');
+			nPostContainer.classList.add('post-container');
+				
+			var nTitle = document.createElement('p');
+			nTitle.classList.add('post-title');
+			nTitle.innerHTML=item.title;
+				
+			var nContent = document.createElement('p');
+			nContent.classList.add('post-content');
+			var paragraphs = item.content.split('\n');
+			var content="";
+			var i=0;
+			for(var paragraph of paragraphs){
+				if(i<paragraphs.length-1){content+=paragraph+'<br/><br/>'}
+				else{content+=paragraph};
+				i++;
+			}
+			nContent.innerHTML=content.trim();
+				
+			nPostContainer.appendChild(nTitle);
+			nPostContainer.appendChild(nContent);
+				
+			nPost.appendChild(nDate);
+			nPost.appendChild(nPostContainer);
+				
+			blogPosts.appendChild(nPost);
+		})
+	})
+}
 var navlinks = document.querySelector('.nav-links');
 var dropdown = document.querySelector('.dropdown');
 dropdown.addEventListener('click', drop);
